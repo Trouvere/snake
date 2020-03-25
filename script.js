f9630260c41da65a3dc3da4c62ba7e77b4783c3f
Mishyn Aliaksei <trouvere777@mail.ru>
2020-03-25 22:17:00 +0300

    Add snake and mouse

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ for (let i = 0; i < excel.length; i++) {
   excel[i].setAttribute("posY", y);
   x++;
 }
+// random position snake, min=3
+function generateSnake() {
+  let posX = Math.round(Math.random() * (10 - 3) + 3);
+  let posY = Math.round(Math.random() * (10 - 3) + 3);
+  return [posX, posY];
+}
+let coordinates = generateSnake();
+let snakeBody = [
+  document.querySelector(
+    '[posX="' + coordinates[0] + '"][posY="' + coordinates[1] + '"]'
+  ),
+  document.querySelector(
+    '[posX="' + (coordinates[0] - 1) + '"][posY="' + coordinates[1] + '"]'
+  ),
+  document.querySelector(
+    '[posX="' + (coordinates[0] - 2) + '"][posY="' + coordinates[1] + '"]'
+  )
+];
+for (let i = 0; i < snakeBody.length; i++) {
+  snakeBody[i].classList.add("snakeBody");
+}
+snakeBody[0].classList.add("snakeHead");
+// console.log(coordinates);
+// console.log(snakeBody);
+let mouse;
+
+function createMouse() {
+  function generateMouse() {
+    let posX = Math.round(Math.random() * (10 - 3) + 3);
+    let posY = Math.round(Math.random() * (10 - 3) + 3);
+    return [posX, posY];
+  }
+  let coordinatesMouse = generateMouse();
+  //   console.log(coordinatesMouse);
+  mouse = document.querySelector(
+    '[posX="' + coordinatesMouse[0] + '"][posY="' + coordinatesMouse[1] + '"]'
+  );
+  while (mouse.classList.contains("snakeBody")) {
+    let coordinatesMouse = generateMouse();
+    mouse = document.querySelector(
+      '[posX="' + coordinatesMouse[0] + '"][posY="' + coordinatesMouse[1] + '"]'
+    );
+  }
+  mouse.classList.add("mouse");
+}
+createMouse();
